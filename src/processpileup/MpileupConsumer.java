dc2a2781a51a7b23ddd25526b1e37570fdc2c8da
Radoslaw Suchecki <r.suchecki@gmail.com>
2016-03-11 16:41:15 +1030

    Changes to base calling, implemented IUPAC handling, partial indel handling (to be continued)

---
+++ src/processpileup/MpileupConsumer.java
@@ -0,0 +1,289 @@
+/*
+ * Copyright 2016 rad.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package processpileup;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.concurrent.BlockingQueue;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.zip.GZIPInputStream;
+import shared.Reporter;
+
+/**
+ *
+ * @author rad
+ */
+public class MpileupConsumer implements Runnable {
+
+    private final BlockingQueue<ArrayList<String>> inputQueue;
+    private final int minCoverageThreshold;
+    private final int maxCoverageThreshold;
+    private final int minSamples;
+    private final int maxPercAlternative;
+    private final String TOOL_NAME;
+
+    public MpileupConsumer(BlockingQueue<ArrayList<String>> inputQueue, int minCoverageThreshold,
+        int maxCoverageThreshold, int minSamples, int maxPercAlternative, String TOOL_NAME) {
+        this.inputQueue = inputQueue;
+        this.minCoverageThreshold = minCoverageThreshold;
+        this.maxCoverageThreshold = maxCoverageThreshold;
+        this.minSamples = minSamples;
+        this.maxPercAlternative = maxPercAlternative;
+        this.TOOL_NAME = TOOL_NAME;
+    }
+
+    @Override
+    public void run() {
+        try {
+            ArrayList<String> list;
+            while (!(list = inputQueue.take()).isEmpty()) {
+                for (String line : list) {
+                    String[] toks = line.split("\t");
+                    int r = 0;
+                    //tos 0,1,2 are ref, position, refbase
+                    char refBase = toks[2].charAt(0);
+                    StringBuilder coveragesSB = new StringBuilder("COUNTS");
+                    StringBuilder callsSB = new StringBuilder("CALLS");
+                    StringBuilder common = new StringBuilder("\t");
+                    common.append(toks[0]).append("\t").append(toks[1]).append("\t").append(toks[2]);
+                    callsSB.append(common);
+                    coveragesSB.append(common);
+                    boolean calledDifferentBases = false;
+//                    String lastCalledBase = "N";
+                    char lastCalledBase = '?';
+
+                    int samplesWithinCoverage = 0;
+                    for (int i = 3; i < toks.length; i += 3) {
+                        try {
+                            int coverage = Integer.parseInt(toks[i]);
+                            if (coverage >= minCoverageThreshold && coverage <= maxCoverageThreshold) {
+                                samplesWithinCoverage++;
+                            }
+                            int[] bases = getBaseCounts(toks[i + 1], refBase);
+                            coveragesSB.append("\t");
+//                            String calledBase = callBase(bases, maxPercAlternative, minCoverageThreshold);
+
+                            char calledBase = getIUPAC(bases, coverage, maxPercAlternative, minCoverageThreshold);
+//                            if (calledBase.matches("A|T|C|G")) {
+                            if (calledBase != '?') {
+//                                if (!calledBase.equals(lastCalledBase) && lastCalledBase.matches("A|T|C|G")) {
+                                if (calledBase != lastCalledBase && lastCalledBase != '?') {
+                                    calledDifferentBases = true;
+                                }
+                                lastCalledBase = calledBase;
+                            }
+                            callsSB.append("\t").append(calledBase);
+                            for (int j = 1; j < bases.length; j++) {
+                                coveragesSB.append(bases[j]);
+                                if (j < bases.length - 1) {
+                                    coveragesSB.append(",");
+                                }
+                            }
+                        } catch (ArrayIndexOutOfBoundsException e) {
+                            Reporter.report("[FATAL]", "Array index out of bounds - likely cause: mismatch between samples given and pileup file content", TOOL_NAME);
+                            System.exit(1);
+                        }
+                    }
+                    if (samplesWithinCoverage >= minSamples && calledDifferentBases) {
+                        System.out.println(coveragesSB);
+                        System.out.println(callsSB);
+                    }
+                }
+            }
+            inputQueue.put(new ArrayList<String>()); //inform other threads
+        } catch (InterruptedException ex) {
+            ex.printStackTrace();
+        }
+
+    }
+
+    private char callBase(int[] bases, int maxPercAlternativeAllowed, int minCoverageThreshold) {
+        int maxCov = 0;
+        int maxPos = 0;
+        int totalDepth = 0;
+        //Identify "major allele"
+        for (int i = 1; i < bases.length; i++) {
+            if (bases[i] > maxCov) {
+                maxCov = bases[i];
+                maxPos = i;
+            }
+            totalDepth += bases[i];
+        }
+
+        double percentOfOtherBases = 1 - ((double) maxCov / totalDepth);
+
+        if (maxCov >= minCoverageThreshold) {
+            if (percentOfOtherBases > maxPercAlternativeAllowed / 100) {
+                return 'N'; //
+            }
+            return getBase(maxPos);
+        }
+        if (maxCov == 0) {
+            return ' ';
+        }
+        //If uncertain
+        return '?';
+    }
+
+//    private char getIUPAC(int [] callCounts, int ) {
+//        
+//    }
+    private char getBase(int encoded) {
+        switch (encoded) {
+            case 1:
+                return 'A';
+            case 2:
+                return 'C';
+            case 3:
+                return 'G';
+            case 4:
+                return 'T';
+            default:
+                return 'N';
+        }
+    }
+
+    /**
+     * |------------------------+----------------------| | UPAC nucleotide code | Base |
+     * |------------------------+----------------------| | A | Adenine | | C | Cytosine | | G | Guanine | | T (or U) |
+     * Thymine (or Uracil) | | R | A or G | | Y | C or T | | S | G or C | | W | A or T | | K | G or T | | M | A or C | |
+     * B | C or G or T | | D | A or G or T | | H | A or C or T | | V | A or C or G | | N | any base | | . or - | gap |
+     * i.e. insertion in the reference? |------------------------+----------------------|
+     */
+    private char getIUPAC(int[] encoded, int totalDepth, int maxErrorPercent, int minCoverageThreshold) {
+        boolean tt[] = new boolean[5];
+        int minCoverage = Math.max((int) Math.ceil(totalDepth * (float) maxErrorPercent / 100), minCoverageThreshold);
+        for (int i = 0; i < encoded.length; i++) {
+            if (encoded[i] >= minCoverage) {
+                tt[i] = true;
+            }
+        }
+        if (!tt[1] && !tt[2] && !tt[3] && !tt[4]) {
+            return '?';
+        } else if (tt[1] && !tt[2] && !tt[3] && !tt[4]) { //A
+            return 'A';
+        } else if (!tt[1] && tt[2] && !tt[3] && !tt[4]) { //C
+            return 'C';
+        } else if (!tt[1] && !tt[2] && tt[3] && !tt[4]) { //G
+            return 'G';
+        } else if (!tt[1] && !tt[2] && !tt[3] && tt[4]) { //T
+            return 'T';
+        } else if (tt[1] && !tt[2] && tt[3] && !tt[4]) { //A or G 
+            return 'R';
+        } else if (!tt[1] && tt[2] && !tt[3] && tt[4]) { //C or T 
+            return 'Y';
+        } else if (!tt[1] && tt[2] && tt[3] && !tt[4]) { //G or C 
+            return 'S';
+        } else if (tt[1] && !tt[2] && !tt[3] && tt[4]) { //A or T 
+            return 'W';
+        } else if (!tt[1] && !tt[2] && tt[3] && tt[4]) { //G or T 
+            return 'K';
+        } else if (tt[1] && tt[2] && !tt[3] && !tt[4]) { //A or C 
+            return 'M';
+        } else if (!tt[1] && tt[2] && tt[3] && tt[4]) { //C or G or T
+            return 'B';
+        } else if (tt[1] && !tt[2] && tt[3] && tt[4]) { //A or G or T
+            return 'D';
+        } else if (tt[1] && tt[2] && !tt[3] && tt[4]) { //A or C or T
+            return 'H';
+        } else if (tt[1] && tt[2] && tt[3] && !tt[4]) { //A or C or G
+            return 'V';
+        } else if (tt[1] && tt[2] && tt[3] && tt[4]) {   //any base
+            return 'N';
+        }
+        return '#'; //should never happen
+    }
+
+    private int[] getBaseCounts(String s, char refBase) {
+//        s = s.replaceAll("\\.|,", String.valueOf(refBase)).replaceAll("\\*", "I").replaceAll("\\^.|\\$", "");
+        int[] bases = new int[7]; // ?,A,C,G,T,insRef,delRef 
+        StringBuilder lenInsRef = null;
+        StringBuilder lenDelRef = null;
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            switch (c) {
+                case '.':
+                case ',':
+                    c = refBase;
+            }
+
+            //Not processing an indel
+            if (lenInsRef == null && lenDelRef == null) {
+                switch (c) {
+                    case 'A':
+                    case 'a':
+                        bases[1]++;
+                        break;
+                    case 'C':
+                    case 'c':
+                        bases[2]++;
+                        break;
+                    case 'G':
+                    case 'g':
+                        bases[3]++;
+                        break;
+                    case 'T':
+                    case 't':
+                        bases[4]++;
+                        break;
+                    case '^': //aligned fragment start
+                    case '$': //aligned fragment end
+                        break;
+                    case '+': //insertion in the reference indicator, pattern: `\+[0-9]+[ACGTNacgtn]+'            
+                        lenInsRef = new StringBuilder();
+                        break;
+                    case '-': //deletion  in the reference indicator, pattern: `\-[0-9]+[ACGTNacgtn]+'            
+                        lenDelRef = new StringBuilder();
+                        break;
+                    default:
+//                    bases[0]++;
+                }
+            } else {//processing an indel                        
+                if (lenInsRef != null) {
+                    if (Character.isDigit(c)) {
+                        lenInsRef.append(c);
+                    } else {
+                        int indelLen = Integer.parseInt(lenInsRef.toString());
+                        lenInsRef = null;
+                        i += indelLen - 1;
+                        bases[5]++;
+                    }
+                }
+                if (lenDelRef != null) {
+                    if (Character.isDigit(c)) {
+                        lenDelRef.append(c);
+                    } else {
+                        int indelLen = Integer.parseInt(lenDelRef.toString());
+                        lenDelRef = null;
+                        i += indelLen - 1;
+                        bases[6]++;
+                    }
+                }
+            }
+        }
+        return bases;
+    }
+}
---
+++ src/processpileup/MpileupCounts.java
@@ -0,0 +1,323 @@
+/*
+ * Copyright 2015 Australian Centre For Plant Functional Genomics
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package processpileup;
+
+import argparser.ArgParser;
+import argparser.Opt;
+import argparser.OptSet;
+import argparser.PositionalOpt;
+import gbssplit.SampleBuffer;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Future;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.zip.GZIPInputStream;
+import kmerextender.KmerExtender;
+import kmerextender.PairMerMapPopulatorConsumer;
+import shared.InputReaderProducer;
+import shared.Reporter;
+
+/**
+ *
+ * @author Radoslaw Suchecki <radoslaw.suchecki@adelaide.edu.au>
+ */
+public class MpileupCounts {
+
+    private final String TOOL_NAME;
+    boolean IGNORE_SPLICING = true;
+//    private String INPUT_FILE;
+
+//    private ArrayList<String> SAMPLE_NAMES;
+    private final int HELP_WIDTH = 200;
+
+    public MpileupCounts(String[] args, String callerName, String toolName) {
+        TOOL_NAME = callerName + " " + toolName;
+
+        OptSet optSet = populateOptSet();
+        ArgParser argParser = new ArgParser();
+        argParser.processArgs(args, optSet, true, callerName, HELP_WIDTH);
+        parallelMpileupProcessing(optSet);
+    }
+
+    private OptSet populateOptSet() {
+        OptSet optSet = new OptSet("Process multi-sample mpileup input. Base calls printed to stdout prefixed with ^CALLS\\t, "
+            + "base counts printed to stdout prefixed by ^COUNTS\\t");
+        //INPUT
+        optSet.setListingGroupLabel("[Input settings]");
+        optSet.addOpt(new Opt('n', "sample-names", "space separated sample names, order must correspond to mpileup input content").setMinValueArgs(2).setMaxValueArgs(Integer.MAX_VALUE).setRequired(true));
+        optSet.addOpt(new Opt('c', "min-coverage-per-sample", "Minimum coverage required for a sample to be processed/considered", 1).setMinValue(1).setDefaultValue(2));
+        optSet.addOpt(new Opt('C', "max-coverage-per-sample", "Maximum coverage allowed for a sample to be processed/considered", 1).setMinValue(1).setDefaultValue(1000));
+        optSet.addOpt(new Opt('s', "min-samples-within-coverage", "Minimum samples within coverage thresholds required to produce ouput", 1).setMinValue(1).setDefaultValue(2));
+        optSet.addOpt(new Opt('M', "max-percent-alternative-alleles", "Maximum percentage of alternative alleles for a base to be called ", 1).setMinValue(1).setDefaultValue(5));
+        optSet.addOpt(new Opt('p', "pileup-file", "Input (m)pileup file, alternatively use stdin", 1));
+        optSet.addOpt(new Opt('t', "threads", "Max number of threads to be used", 1).setMinValue(1).setDefaultValue(1).setMaxValue(Runtime.getRuntime().availableProcessors()));
+        optSet.addOpt(new Opt('U', "in-buffer-size", "Size of buffers put on in-queue ", 1024, 128, 8192));
+        optSet.addOpt(new Opt('Q', "in-queue-capacity", "Maximum number of buffers put on queue for processing threads to pick-up",
+            64, 1, 256));
+//        String headerNote = "Can be useful for external parallization (print header once)";
+//        optSet.addOpt(new Opt('H', "header-only", "Print header and exit").addFootnote(1, TOOL_NAME));
+//        optSet.addOpt(new Opt('N', "no-header", "Do not print the header"));
+
+        return optSet;
+    }
+
+    private CharSequence getHeader(ArrayList<String> sampleIds) {
+        StringBuilder header = new StringBuilder();
+        header.append("Ref\tPos\tRefBase");
+        for (String sampleId : sampleIds) {
+            header.append("\t").append(sampleId);
+        }
+//        System.err.print("Ref\tPos");
+//        for (int i = 0; i < sampleIds.size(); i++) {
+//            System.err.print("\tA,C,G,T");            
+//        }
+//        System.err.println("");
+        return header;
+    }
+
+    private void parallelMpileupProcessing(OptSet optSet) {
+
+        ArrayList<String> SAMPLE_NAMES = (ArrayList<String>) optSet.getOpt("n").getValues();
+        int minCoverageThreshold = (int) optSet.getOpt("c").getValueOrDefault();
+        int maxCoverageThreshold = (int) optSet.getOpt("C").getValueOrDefault();
+        int minSamples = (int) optSet.getOpt("s").getValueOrDefault();
+        int maxPercAlternative = (int) optSet.getOpt("M").getValueOrDefault();
+        String inputFile = (String) optSet.getOpt("p").getValueOrDefault();
+        int READER_BUFFER_SIZE = (int) optSet.getOpt("U").getValueOrDefault();
+        int IN_Q_CAPACITY = (int) optSet.getOpt("Q").getValueOrDefault();
+        int MAX_THREADS = (int) optSet.getOpt("t").getValueOrDefault();
+
+        System.out.println("COUNTS\t"+getHeader(SAMPLE_NAMES));
+        System.out.println("CALLS\t"+getHeader(SAMPLE_NAMES));
+
+//        mpileupCounts(PILEUP_FILE, minCoverageThreshold, maxCoverageThreshold, minSamples, maxPercAlternative);
+        try {
+            //READ INPUT AND POPULATE PairMers MAP
+            int threads = MAX_THREADS;
+            Reporter.report("[INFO]", "Allocated " + threads + " thread(s) to mpileup processing", TOOL_NAME);
+            ArrayList<Future<?>> futures = new ArrayList<>(threads + 1);
+            final ExecutorService readAndPopulateExecutor = new ThreadPoolExecutor(threads + 1, threads + 1, 0L, TimeUnit.MILLISECONDS, new LinkedBlockingQueue<Runnable>());
+
+            BlockingQueue inputQueue = new ArrayBlockingQueue(IN_Q_CAPACITY);
+
+            //SPAWN INPUT READING THREAD            
+            InputReaderProducer inputReaderProducer = new InputReaderProducer(inputQueue, inputFile, null, TOOL_NAME, READER_BUFFER_SIZE);
+
+            Future<?> future = readAndPopulateExecutor.submit(inputReaderProducer);
+            futures.add(future);
+            
+
+            //ENSURING WE KNOW THE INPUT FORMAT BEFORE CONSUMER THREADS ARE SPAWNED
+            long timeStart = System.currentTimeMillis();
+            int count = 0;
+            while (inputReaderProducer.getGuessedInputFormat() == null) {
+                try {
+                    //IF nothing happens after 5 seconds
+                    if (System.currentTimeMillis() - timeStart > 5000 && (count++ % 50 == 0)) {
+                        Reporter.report("[WARNING]", "Stuck or waiting for standard input stream...", TOOL_NAME);
+                    }
+                    Thread.sleep(100); //wait for 1/10 of a second
+                } catch (InterruptedException ex) {
+                }
+            }
+
+            //SPAWN CONSUMER THREADS 
+            for (int i = 0; i < threads; i++) {
+                MpileupConsumer consumer = new MpileupConsumer(inputQueue, minCoverageThreshold, maxCoverageThreshold, minSamples, maxPercAlternative, TOOL_NAME);
+                futures.add(readAndPopulateExecutor.submit(consumer));
+            }
+            readAndPopulateExecutor.shutdown();
+            try {
+                for (Future<?> f : futures) {
+                    f.get(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
+                }
+                readAndPopulateExecutor.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
+            } catch (InterruptedException e) {
+                Reporter.report("[ERROR]", "interrupted exception!", TOOL_NAME);
+            } catch (ExecutionException ex) {
+                Reporter.report("[ERROR]", "execution exception!", TOOL_NAME);
+                ex.printStackTrace();
+
+            } catch (TimeoutException ex) {
+                Logger.getLogger(KmerExtender.class.getName()).log(Level.SEVERE, null, ex);
+                Reporter.report("[ERROR]", "timeout exception!", TOOL_NAME);
+            }
+
+        } catch (OutOfMemoryError e) {
+            Reporter.report("[ERROR]", "Out of memory error!", TOOL_NAME);
+            System.exit(1);
+        }
+        Reporter.report("[INFO]", "Done!", TOOL_NAME);
+    }
+
+//    private void mpileupCounts(String inputFile, int minCoverageThreshold, int maxCoverageThreshold, 
+//        int minSamples, int maxPercAlternative) {        
+//        BufferedReader content = null;
+//        try {
+//            if (inputFile == null || inputFile.equals("-")) { //READSTDIN
+//                content = new BufferedReader(new InputStreamReader(System.in), READER_BUFFER_SIZE);
+//            } else if (inputFile.endsWith(".gz")) {
+//                InputStream gzipStream = new GZIPInputStream(new FileInputStream(inputFile));
+//                content = new BufferedReader(new InputStreamReader(gzipStream, "UTF-8"), READER_BUFFER_SIZE);
+//            } else {
+//                content = new BufferedReader(new FileReader(new File(inputFile)), READER_BUFFER_SIZE);
+//            }
+//            String line;
+//            while ((line = content.readLine()) != null && !line.isEmpty()) {
+//                String[] toks = line.split("\t");
+//                int r = 0;
+//                //tos 0,1,2 are ref, position, refbase                                
+//                char refBase = toks[2].charAt(0);
+//                StringBuilder coveragesSB = new StringBuilder();
+//                StringBuilder callsSB = new StringBuilder();
+//                StringBuilder common = new StringBuilder();
+//                common.append(toks[0]).append("\t").append(toks[1]).append("\t").append(toks[2]);
+//                callsSB.append(common);
+//                coveragesSB.append(common);
+//                boolean calledDifferentBases = false;
+//                char lastCalledBase = 'N';
+//                int samplesWithinCoverage = 0;
+//                for (int i = 3; i < toks.length; i += 3) {
+//                    try {
+//                        int coverage = Integer.parseInt(toks[i]);
+//                        if(coverage >= minCoverageThreshold && coverage <= maxCoverageThreshold) {
+//                            samplesWithinCoverage++;
+//                        }
+//                        int[] bases = getBaseCounts(toks[i + 1], refBase);
+//                        coveragesSB.append("\t");
+//                        char calledBase = callBase(bases, maxPercAlternative, minCoverageThreshold);
+//                        if(calledBase != 'N') { 
+//                            if(calledBase != lastCalledBase && lastCalledBase != 'N') {
+//                                calledDifferentBases = true;
+//                            }
+//                            lastCalledBase = calledBase;
+//                        } 
+//                        callsSB.append("\t").append(calledBase);
+//                        for (int j = 1; j < bases.length; j++) {
+//                            coveragesSB.append(bases[j]);
+//                            if (j < bases.length - 1) {
+//                                coveragesSB.append(",");                                
+//                            }
+//                        }
+//                    } catch (ArrayIndexOutOfBoundsException e) {
+//                        Reporter.report("[FATAL]", "Array index out of bounds - likely cause: mismatch between samples given and pileup file content", TOOL_NAME);
+//                        System.exit(1);
+//                    }
+//                }
+//                if (samplesWithinCoverage >= minSamples && calledDifferentBases) {
+//                    System.err.println(coveragesSB);
+//                    System.out.println(callsSB);
+//                }
+//            }
+//
+//        } catch (FileNotFoundException ex) {
+//            Reporter.report("[ERROR]", "File not found exception: " + ex.getMessage(), TOOL_NAME);
+//            System.exit(1);
+//        } catch (IOException ex) {
+//            System.err.println(ex.getMessage());
+//        } finally {
+//            try {
+//                if (content != null) {
+//                    content.close();
+//                }
+//            } catch (IOException ex) {
+//                System.err.println(ex.getMessage());
+//            }
+//        }
+//    }
+//
+//    private char callBase(int[] bases, int maxPercAlternative, int minCoverageThreshold) {
+//        int maxCov=0;
+//        int maxPos=0;      
+//        int totalDepth=0;
+//        for (int i = 1; i < bases.length; i++) {
+//            if(bases[i]>maxCov) {
+//                maxCov=bases[i];
+//                maxPos=i;
+//            }          
+//            totalDepth+=bases[i];
+//        }
+//              
+//        double percOfOtherBases = 1 - ((double) maxCov / totalDepth);
+//        if (maxCov >= minCoverageThreshold && percOfOtherBases > maxPercAlternative / 100) {
+//            return 'N';
+//        }
+//        switch (maxPos) {
+//            case 1: return 'A';
+//            case 2: return 'C';
+//            case 3: return 'G';
+//            case 4: return 'T';
+//            default: return 'N';
+//        }
+//        
+//    }
+//    
+//    private int[] getBaseCounts(CharSequence s, char refBase) {
+//        int[] bases = new int[5];
+//        for (int i = 0; i < s.length(); i++) {
+//            char c = s.charAt(i);
+//            switch (c) {
+//                case '.':
+//                case ',':
+//                    c = refBase;
+//                    break;
+//            }
+//
+//            switch (c) {
+//                case 'A':
+//                case 'a':
+//                    bases[1]++;
+//                    break;
+//                case 'C':
+//                case 'c':
+//                    bases[2]++;
+//                    break;
+//                case 'G':
+//                case 'g':
+//                    bases[3]++;
+//                    break;
+//                case 'T':
+//                case 't':
+//                    bases[4]++;
+//                    break;
+//                default:
+////                    bases[0]++;
+//            }
+//
+//        }
+//        return bases;
+//    }
+}
