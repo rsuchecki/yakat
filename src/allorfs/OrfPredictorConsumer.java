c549173dc3fe008a69e5483664fe9433a060e1e1
Radoslaw Suchecki <r.suchecki@gmail.com>
2018-09-28 10:20:00 +0930

    code cleanup sanity checks,

--- src/allorfs/OrfPredictorConsumer.java
+++ src/allorfs/OrfPredictorConsumer.java
@@ -19,6 +19,7 @@ import java.io.PrintStream;
 import java.text.NumberFormat;
 import java.util.ArrayList;
 import java.util.concurrent.BlockingQueue;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import shared.Orf;
 import shared.Reporter;
@@ -37,14 +38,23 @@ public class OrfPredictorConsumer implements Runnable {
     private final PrintStream bufferedOut;
     private final boolean translate;
     private final boolean requireStop;
+    private final boolean strandMatcherPlus;
+    private final boolean strandMatcherMinus;
 
-    public OrfPredictorConsumer(BlockingQueue<ArrayList<Sequence>> inputQueue, String TOOL_NAME, int minLength, PrintStream bufferedOut, boolean translate, boolean requireStop) {
+    public OrfPredictorConsumer(BlockingQueue<ArrayList<Sequence>> inputQueue, String TOOL_NAME, int minLength, PrintStream bufferedOut,
+            boolean translate, boolean requireStop, String strand) {
         this.inputQueue = inputQueue;
         this.TOOL_NAME = TOOL_NAME;
         this.minLength = minLength;
         this.bufferedOut = bufferedOut;
         this.translate = translate;
         this.requireStop = requireStop;
+        strandMatcherPlus = Pattern.compile("both|plus|f(orward)?|fwd|\\+", Pattern.CASE_INSENSITIVE).matcher(strand).matches();
+        strandMatcherMinus = Pattern.compile("both|minus|rev|r(everse)?|\\-", Pattern.CASE_INSENSITIVE).matcher(strand).matches();
+        if(!strandMatcherPlus && !strandMatcherMinus) {
+            Reporter.report("[FATAL]", "Ambiguous specpeciofication of -s / --strand by the user: " + strand, TOOL_NAME);
+            System.exit(1);
+        }        
     }
 
     @Override
@@ -54,7 +64,9 @@ public class OrfPredictorConsumer implements Runnable {
         Pattern startStopCodonsReverse = Pattern.compile("CAT|((TT|TC|CT)A)", Pattern.CASE_INSENSITIVE);
         Pattern stopCodonsForward = Pattern.compile("T(AA|AG|GA)", Pattern.CASE_INSENSITIVE);
         Pattern stopCodonsReverse = Pattern.compile("(TT|TC|CT)A", Pattern.CASE_INSENSITIVE);
+        
 
+                
         try {
             while (!(list = inputQueue.take()).isEmpty()) {
                 for (Sequence sequence : list) {
@@ -64,30 +76,32 @@ public class OrfPredictorConsumer implements Runnable {
                     for (Orf orf : orfs) {
 //                System.out.printf("%8s%12d%12d%3d%12d\n",orf.getParenId(),orf.getFrom(),orf.getTo(),orf.getFrame(),orf.getLength());
                         if (!requireStop || orf.hasStopCodon()) {
-                            CharSequence seq = sequence.getSequenceString().subSequence(orf.getFrom() - 1, orf.getTo());
-                            StringBuilder sb = new StringBuilder(orf.getFastaHeader());
-                            sb.append(System.lineSeparator());
-                            if (orf.getFrame() < 0) {
-                                seq = SequenceOps.getReverseComplement(seq);
+                            if ((strandMatcherPlus && orf.getFrame() > 0) || (strandMatcherMinus && orf.getFrame() < 0)) {
+                                CharSequence seq = sequence.getSequenceString().subSequence(orf.getFrom() - 1, orf.getTo());
+                                StringBuilder sb = new StringBuilder(orf.getFastaHeader());
+                                sb.append(System.lineSeparator());
+                                if (orf.getFrame() < 0) {
+                                    seq = SequenceOps.getReverseComplement(seq);
+                                }
+                                if (translate) {
+                                    sb.append(SequenceOps.translate(seq));
+                                } else {
+                                    sb.append(seq);
+                                }
+                                int len = orf.getTo() - orf.getFrom() + 1;
+                                int seqLength = seq.length();
+                                if (seqLength != len) {
+                                    Reporter.report("[BUG]", "ORF length mismatch", TOOL_NAME);
+                                    System.err.println(sb);
+                                    System.exit(1);
+                                }
+                                bufferedOut.println(sb);
+                                count++;
                             }
-                            if (translate) {
-                                sb.append(SequenceOps.translate(seq));
-                            } else {
-                                sb.append(seq);
-                            }
-                            int len = orf.getTo()-orf.getFrom()+1;
-                            int seqLength = seq.length();
-                            if(seqLength != len) {
-                                Reporter.report("[BUG]", "ORF length mismatch", TOOL_NAME);
-                                System.err.println(sb);
-                                System.exit(1);
-                            }
-                            bufferedOut.println(sb);
-                            count++;
                         }
                     }
-                    Reporter.report("[INFO]", NumberFormat.getInstance().format(orfs.size()) + " ORFs found on "+ sequence.getId(), TOOL_NAME);
-                    Reporter.report("[INFO]", "Outputtig "+NumberFormat.getInstance().format(count)+ " out of "+NumberFormat.getInstance().format(orfs.size()) +" ORFs identified on " + sequence.getId(), TOOL_NAME);
+                    Reporter.report("[INFO]", NumberFormat.getInstance().format(orfs.size()) + " ORFs found on " + sequence.getId(), TOOL_NAME);
+                    Reporter.report("[INFO]", "Outputtig " + NumberFormat.getInstance().format(count) + " out of " + NumberFormat.getInstance().format(orfs.size()) + " ORFs identified on " + sequence.getId(), TOOL_NAME);
                 }
             }
             inputQueue.put(new ArrayList<>()); //inform other threads
